5a34b5ec96fcf6dbaadd4cc496df256dab427325
Erica Travers <etravers@student.bridgew.edu>
2019-11-14 12:52:50 -0500

    Initial commit

---
+++ src/main/java/bsu/comp152/Main.java
@@ -0,0 +1,32 @@
+package bsu.comp152;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import java.io.IOException;
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        Parent root = null;
+        var loc = getClass().getResource("Main.fxml");
+        try {
+            root = FXMLLoader.load(loc);
+        }catch (IOException e){
+            System.out.println("Couldn't Find FXML file!!!!!!");
+            e.printStackTrace();
+        }
+        Scene windowContents = new Scene(root, 328,412);
+        primaryStage.setScene(windowContents);
+        primaryStage.setTitle("Showing Web Data");
+        primaryStage.show();
+    }
+
+}
