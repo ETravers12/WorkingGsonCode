78bcc81c00ba07ff60b82167c999fbf629d46c17
Erica Travers <etravers@student.bridgew.edu>
2019-11-14 12:53:28 -0500

    Putting code onto github

--- src/main/java/bsu/comp152/Main.java
+++ src/main/java/bsu/comp152/Main.java
@@ -7,6 +7,7 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import java.io.IOException;
 
+// Code
 public class Main extends Application {
 
     public static void main(String[] args) {
